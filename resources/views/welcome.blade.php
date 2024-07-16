4ef4c9f3b21de10814d8ce57a420f9d4e739a50e
Thielander <alexander@thiele.es>
2024-07-16 12:47:24 +0200

    change blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,7 @@
             <button type="submit" class="btn btn-primary mt-3">Search</button>
         </form>
         @isset($product)
+        dd($product)
             <div class="mt-5">
                 <h2>Product Information</h2>
                 <p><strong>Name:</strong> {{ $product['product_name'] ?? 'N/A' }}</p>
